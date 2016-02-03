e604faa3193756139f44664215dd52b9a78441ed
sorasak <nightontherock@gmail.com>
2016-02-03 11:13:24 +0700

    Create MyManage Class Successful

---
+++ app/src/main/java/kawinpart/sorasak/weightcheck/MyManage.java
@@ -0,0 +1,29 @@
+package kawinpart.sorasak.weightcheck;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+/**
+ * Created by User on 3/2/2559.
+ */
+public class MyManage {
+
+    //Explicit
+    private MyOpenHelper objMyOpenHelper;
+    private SQLiteDatabase writeSqliteDatabase, readSqlLiteDatabase;
+
+
+    public MyManage(Context context) {
+
+        //Create & Connected
+        objMyOpenHelper = new MyOpenHelper(context);
+        writeSqliteDatabase = objMyOpenHelper.getWritableDatabase();
+        readSqlLiteDatabase = objMyOpenHelper.getReadableDatabase();
+
+
+
+
+
+    } //Constructor
+
+}  // Main Class
