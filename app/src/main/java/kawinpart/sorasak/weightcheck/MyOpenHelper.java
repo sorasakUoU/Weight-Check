e0a3f4a60c2eb6dad8464c0223dabe4db7c3f5dc
sorasak <nightontherock@gmail.com>
2016-02-03 11:02:42 +0700

    Create MyOpenHelper Successful

--- app/src/main/java/kawinpart/sorasak/weightcheck/MyOpenHelper.java
+++ app/src/main/java/kawinpart/sorasak/weightcheck/MyOpenHelper.java
@@ -1,11 +1,34 @@
 package kawinpart.sorasak.weightcheck;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by User on 3/2/2559.
  */
-public class MyOpenHelper {
+public class MyOpenHelper extends SQLiteOpenHelper{
 
-    public MyOpenHelper() {
+    //Explicit
+    public static final String database_name = "Weight.db"; //ประกาศค่าคงที่ ที่ไม่สามารถแก้ไขได้
+    private static final int database_version = 1;
+    private static final String create_table_weight = "create table weightTABLE (" +
+            "_id integer primary key, " +
+            "Date text, " +
+            "Weight text);";
+
+
+    public MyOpenHelper(Context context) {
+        super(context, database_name, null, database_version);
     } //Constructor
 
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(create_table_weight);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+
+    }
 } //Main Class
